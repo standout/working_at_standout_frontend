d5ad137314ab73bcd9cfda68061276a9aacb90c3
Vamic <made.swede@live.com>
2016-08-20 15:50:39 +0200

    More Editing Tests
    Added tests that make sure it fails on duplicate or faulty entries.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,34 @@
 describe('DB API', function () {
+    beforeEach(function (done) {
+        //A bit of delay between tests so the server can keep up
+        setTimeout(function () {
+            done();
+        }, 50);
+    });
     describe('Locations', function () {
+        var oName, oLat, oLng;
+        var fakeLatLng = 10;
+        var fakeName = "DupeCheck";
+        describe('get', function () {
+            it('should get all locations', function (done) {
+                getLocations().done(function (data) {
+                    locations = data;
+                    //Add a fake location for duplicate checking
+                    locations.push({
+                        "id": 1337,
+                        "name": fakeName,
+                        "phone": "000-0000000",
+                        "address": "Bbbbbb 2",
+                        "latitude": fakeLatLng,
+                        "longitude": fakeLatLng,
+                        "category": 1,
+                        "other": ""
+                    });
+                }).always(function () {
+                    done();
+                });
+            });
+        });
         var input = {
             "id": 0,
             "name": "Mocha Test",
@@ -11,12 +40,47 @@ describe('DB API', function () {
             "other": "Added by Mocha JS"
         };
         describe('post', function () {
-            it('should add a new category', function (done) {
-                var method = "add";
+            var method = "add";
+            it('should add a new location', function (done) {
                 sendLocation(method, input).done(function (msg, response) {
-                    assert.ok(response.name === input.name);
                     $.extend(input, response);
+                    locations.push(response);
+                    oName = input.name;
+                    oLat = input.latitude;
+                    oLng = input.longitude;
+                    assert.ok(response.name === input.name);
                 }).fail(function (msg) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
+            it('should fail on duplicate location name', function (done) {
+                input.name = fakeName;
+                sendLocation(method, input).done(function (msg, response) {
+                    assert.fail(msg);
+                }).always(function () {
+                    input.name = oName;
+                    input.latitude = oLat;
+                    input.longitude = oLng;
+                    done();
+                });
+            });
+            it('should fail on duplicate location coordinates', function (done) {
+                input.latitude = fakeLatLng;
+                input.longitude = fakeLatLng;
+                sendLocation(method, input).done(function (msg, response) {
+                    assert.fail(msg);
+                }).always(function () {
+                    input.name = oName;
+                    input.latitude = oLat;
+                    input.longitude = oLng;
+                    done();
+                });
+            });
+            it('should fail on empty name', function (done) {
+                input.name = "";
+                sendLocation(method, input).done(function (msg, response) {
                     assert.fail(msg);
                 }).always(function () {
                     done();
@@ -25,9 +89,14 @@ describe('DB API', function () {
         });
         describe('put', function () {
             var method = "update";
-            input.name = "Mocha Updated Test";
             it('should update a location in the database', function (done) {
+                input.name = "Mocha Updated Test";
                 sendLocation(method, input).done(function (msg, response) {
+                    $.extend(input, response);
+                    $.extend(locations[locations.length - 1], response);
+                    oName = input.name;
+                    oLat = input.latitude;
+                    oLng = input.longitude;
                     assert.ok(response.name === input.name);
                 }).fail(function (msg) {
                     assert.fail(msg);
@@ -35,11 +104,49 @@ describe('DB API', function () {
                     done();
                 });
             });
+            it('should fail on duplicate location name', function (done) {
+                input.name = fakeName;
+                sendLocation(method, input).done(function (msg, response) {
+                    assert.fail(msg);
+                }).always(function () {
+                    input.name = oName;
+                    input.latitude = oLat;
+                    input.longitude = oLng;
+                    done();
+                });
+            });
+            it('should fail on duplicate location coordinates', function (done) {
+                input.latitude = fakeLatLng;
+                input.longitude = fakeLatLng;
+                sendLocation(method, input).done(function (msg, response) {
+                    assert.fail(msg);
+                }).always(function () {
+                    input.name = oName;
+                    input.latitude = oLat;
+                    input.longitude = oLng;
+                    done();
+                });
+            });
+            it('should fail on empty name', function (done) {
+                input.name = "";
+                sendLocation(method, input).done(function (msg, response) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
         });
+        var method = "delete";
         describe('delete', function () {
             it('should delete a location from the database', function (done) {
-                var method = "delete";
                 sendLocation(method, input).fail(function (msg) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
+            it('should fail on bad id', function (done) {
+                sendCategory(method, 0).done(function (msg) {
                     assert.fail(msg);
                 }).always(function () {
                     done();
@@ -48,15 +155,42 @@ describe('DB API', function () {
         });
     });
     describe('Categories', function () {
+        describe('get', function () {
+            it('should get all categories', function (done) {
+                getCategories().done(function (data) {
+                    categories = data;
+                }).fail(function () {
+                    assert.fail();
+                }).always(function () {
+                    done();
+                });
+            });
+        });
         var input = {};
         describe('post', function () {
+            var method = "add";
             it('should add a new category', function (done) {
-                var method = "add";
                 input.name = "Mocha Test";
                 sendCategory(method, input).done(function (msg, response) {
                     assert.ok(response.name === input.name);
                     $.extend(input, response);
+                    categories.push(response);
                 }).fail(function (msg) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
+            it('should fail on duplicate category', function (done) {
+                sendCategory(method, input).done(function (msg, response) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
+            it('should fail on empty name', function (done) {
+                input.name = "";
+                sendCategory(method, input).done(function (msg, response) {
                     assert.fail(msg);
                 }).always(function () {
                     done();
@@ -65,11 +199,28 @@ describe('DB API', function () {
         });
         describe('put', function () {
             var method = "update";
-            input.name = "Mocha Updated Test";
             it('should update a location in the database', function (done) {
+                input.name = "Mocha Updated Test";
                 sendCategory(method, input).done(function (msg, response) {
                     assert.ok(response.name === input.name);
+                    $.extend(input, response);
+                    $.extend(categories[categories.length - 1], response);
                 }).fail(function (msg) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
+            it('should fail on duplicate category', function (done) {
+                sendCategory(method, input).done(function (msg, response) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
+            it('should fail on empty name', function (done) {
+                input.name = "";
+                sendCategory(method, input).done(function (msg, response) {
                     assert.fail(msg);
                 }).always(function () {
                     done();
@@ -77,9 +228,16 @@ describe('DB API', function () {
             });
         });
         describe('delete', function () {
+            var method = "delete";
             it('should delete a location from the database', function (done) {
-                var method = "delete";
                 sendCategory(method, input).fail(function (msg) {
+                    assert.fail(msg);
+                }).always(function () {
+                    done();
+                });
+            });
+            it('should fail on bad id', function (done) {
+                sendCategory(method, 0).done(function (msg) {
                     assert.fail(msg);
                 }).always(function () {
                     done();
