2bed1b8c67b940a71c57928206fe8ca83291103d
Vamic <made.swede@live.com>
2016-08-20 16:45:25 +0200

    Fixed api for running the file directly
    If there is no hostname available it defaults to localhost

--- content/js/apiscript.js
+++ content/js/apiscript.js
@@ -1,38 +1,39 @@
+var host = window.location.hostname || "localhost";
 function getLocations() {
-    return $.get('http://' + window.location.hostname + ':3000/suppliers');
+    return $.get('http://' + host + ':3000/suppliers');
 }
 function addLocation(input) {
-    return $.post('http://' + window.location.hostname + ':3000/suppliers', input);
+    return $.post('http://' + host + ':3000/suppliers', input);
 }
 function updateLocation(input) {
     return $.ajax({
-        url: 'http://' + window.location.hostname + ':3000/suppliers/' + input.id,
+        url: 'http://' + host + ':3000/suppliers/' + input.id,
         method: 'PUT',
         data: input
     });
 }
 function deleteLocation(id) {
     return $.ajax({
-        url: 'http://' + window.location.hostname + ':3000/suppliers/' + id,
+        url: 'http://' + host + ':3000/suppliers/' + id,
         method: 'DELETE'
     });
 }
 function getCategories() {
-    return $.get('http://' + window.location.hostname + ':3000/categories');
+    return $.get('http://' + host + ':3000/categories');
 }
 function addCategory(input) {
-    return $.post('http://' + window.location.hostname + ':3000/categories', input);
+    return $.post('http://' + host + ':3000/categories', input);
 }
 function updateCategory(input) {
     return $.ajax({
-        url: 'http://' + window.location.hostname + ':3000/categories/' + input.id,
+        url: 'http://' + host + ':3000/categories/' + input.id,
         method: 'PUT',
         data: input
     });
 }
 function deleteCategory(id) {
     return $.ajax({
-        url: 'http://' + window.location.hostname + ':3000/categories/' + id,
+        url: 'http://' + host + ':3000/categories/' + id,
         method: 'DELETE'
     });
 }
