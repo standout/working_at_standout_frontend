2b3381ad9461402294ffaa71ed69775edefb6b43
Eddy Proca <Eddy@vwifi-student-719.lnu.se>
2016-08-24 16:36:24 +0200

    Moved categories into database with edit functionality

--- suppliers.js
+++ suppliers.js
@@ -9,49 +9,78 @@ function showSuppliers() {
 			zoom: 5
 		}),
 		geocoder = new google.maps.Geocoder,
-		marker,
-		lat,
-		long;
+		marker;
 	while (ol.firstChild) {
-		ol.removeChild(ol.firstChild);
+		ol.removeChild(ol.firstChild); //clear list
 	}
 	$.get("http://localhost:3000/suppliers", function (suppliers) {
 		for (i = 0; i < suppliers.length; i += 1) {
-			(function () {
-				li = document.createElement("li");
-				li.appendChild(document.createTextNode("Name: " + suppliers[i].name));
-				deleteButton = document.createElement("input");
-				deleteButton.setAttribute("type", "button");
-				deleteButton.setAttribute("value", "Delete");
-				deleteButton.setAttribute("data-id", suppliers[i].id);
-				deleteButton.addEventListener("click", function () {
-					deleteSupplier(this.getAttribute("data-id"));
-				}, false);
-				li.appendChild(deleteButton);
-				ol.appendChild(li);
-				geocoder.geocode({'address': suppliers[i].address}, function (results, status) {
-					if (status === 'OK') {
-						//map.setCenter(results[0].geometry.location);
-						var marker = new google.maps.Marker({
-							map: map,
-							position: results[0].geometry.location
-						});
-					} else {
-						alert('Geocode was not successful for the following reason: ' + status);
-					}
-				});
-			}());
+			li = document.createElement("li");
+			li.appendChild(document.createTextNode("Name: " + suppliers[i].name + " " + suppliers[i].category));
+			deleteButton = document.createElement("input");
+			deleteButton.setAttribute("type", "button");
+			deleteButton.setAttribute("value", "Delete");
+			deleteButton.setAttribute("data-id", suppliers[i].id);
+			deleteButton.addEventListener("click", function () {
+				deleteSupplier(this.getAttribute("data-id"));
+			}, false);
+			li.appendChild(deleteButton);
+			ol.appendChild(li);
+			geocoder.geocode({'address': suppliers[i].address}, function (results, status) {
+				if (status === 'OK') {
+					//map.setCenter(results[0].geometry.location);
+					var marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location
+					});
+				} else {
+					alert('Geocode was not successful for the following reason: ' + status);
+				}
+			});
 		}
 	});
 }
-function addSupplier(name, address) {
+function showCategories() {
 	"use strict";
-	var geocoder = new google.maps.Geocoder;
+	var i,
+		ol = document.getElementById("categories-list"),
+		li,
+		deleteButton;
+	while (ol.firstChild) {
+		ol.removeChild(ol.firstChild);
+	}
+	$.get("http://localhost:3000/categories", function (categories) {
+		for (i = 0; i < categories.length; i += 1) {
+			li = document.createElement("li");
+			li.appendChild(document.createTextNode(categories[i].category));
+			deleteButton = document.createElement("input");
+			deleteButton.setAttribute("type", "button");
+			deleteButton.setAttribute("value", "Delete");
+			deleteButton.setAttribute("data-id", categories[i].id);
+			deleteButton.addEventListener("click", function () {
+				deleteCategory(this.getAttribute("data-id"));
+			}, false);
+			li.appendChild(deleteButton);
+			ol.appendChild(li);
+		}
+	});
+}
+function addSupplier(name, address, category) {
+	"use strict";
+	//var geocoder = new google.maps.Geocoder;
 	$.post("http://localhost:3000/suppliers", {
 		name: name,
-		address: address
+		address: address,
+		category: category
 	});
 	showSuppliers();
+}
+function addCategory(category) {
+	"use strict";
+	$.post("http://localhost:3000/categories", {
+		category: category
+	});
+	showCategories();
 }
 function deleteSupplier(id) {
 	"use strict";
@@ -61,13 +90,27 @@ function deleteSupplier(id) {
 	});
 	showSuppliers();
 }
+function deleteCategory(id) {
+	"use strict";
+	$.ajax({
+		url: "http://localhost:3000/categories/" + id,
+		type: "DELETE"
+	});
+	showCategories();
+}
 function init() {
 	"use strict";
 	document.getElementById("add-new-supplier").addEventListener("click", function () {
 		var name = document.getElementById("new-supplier-name").value,
-			address = document.getElementById("new-supplier-address").value;
-		addSupplier(name, address);
+			address = document.getElementById("new-supplier-address").value,
+			category = document.getElementById("new-supplier-category").value;
+		addSupplier(name, address, category);
+	}, false);
+	document.getElementById("add-new-category").addEventListener("click", function () {
+		var categoryName = document.getElementById("new-category-name").value;
+		addCategory(categoryName);
 	}, false);
 	showSuppliers();
+	showCategories();
 }
 window.onload = init;
