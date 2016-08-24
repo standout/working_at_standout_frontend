0ed998bfb5c479ded8b1bef05b4c81e875e12f79
Eddy Proca <Eddy@vwifi-student-719.lnu.se>
2016-08-24 16:06:49 +0200

    Allow user to enter a normal address

--- suppliers.js
+++ suppliers.js
@@ -1,19 +1,5 @@
-function addSupplier(name, lat, long) {
-	$.post("http://localhost:3000/suppliers", {
-		name: name,
-		latitude: lat,
-		longitude: long
-	});
-	showSuppliers();
-}
-function deleteSupplier(id) {
-	$.ajax({
-		url: "http://localhost:3000/suppliers/" + id,
-		type: "DELETE"
-	});
-	showSuppliers();
-}
 function showSuppliers() {
+	"use strict";
 	var i,
 		ol = document.getElementById("suppliers-list"),
 		li,
@@ -22,44 +8,65 @@ function showSuppliers() {
 			center: {lat: 62.3875, lng: 16.325556}, //center on Sweden
 			zoom: 5
 		}),
+		geocoder = new google.maps.Geocoder,
 		marker,
 		lat,
 		long;
 	while (ol.firstChild) {
-    	ol.removeChild(ol.firstChild);
+		ol.removeChild(ol.firstChild);
 	}
-	$.get("http://localhost:3000/suppliers", function(suppliers) {
-		for (i = 0; i < suppliers.length; i++) {
-			lat = suppliers[i].latitude;
-			long = suppliers[i].longitude;
-			(function(){
+	$.get("http://localhost:3000/suppliers", function (suppliers) {
+		for (i = 0; i < suppliers.length; i += 1) {
+			(function () {
 				li = document.createElement("li");
 				li.appendChild(document.createTextNode("Name: " + suppliers[i].name));
 				deleteButton = document.createElement("input");
 				deleteButton.setAttribute("type", "button");
 				deleteButton.setAttribute("value", "Delete");
-				deleteButton.setAttribute("data-id", suppliers[i].id)
+				deleteButton.setAttribute("data-id", suppliers[i].id);
 				deleteButton.addEventListener("click", function () {
 					deleteSupplier(this.getAttribute("data-id"));
 				}, false);
 				li.appendChild(deleteButton);
 				ol.appendChild(li);
-				marker = new google.maps.Marker({
-					position: {lat: lat, lng: long},
-					map: map,
-					title: suppliers[i].name //shown as tooltip
+				geocoder.geocode({'address': suppliers[i].address}, function (results, status) {
+					if (status === 'OK') {
+						//map.setCenter(results[0].geometry.location);
+						var marker = new google.maps.Marker({
+							map: map,
+							position: results[0].geometry.location
+						});
+					} else {
+						alert('Geocode was not successful for the following reason: ' + status);
+					}
 				});
 			}());
-			
 		}
 	});
 }
+function addSupplier(name, address) {
+	"use strict";
+	var geocoder = new google.maps.Geocoder;
+	$.post("http://localhost:3000/suppliers", {
+		name: name,
+		address: address
+	});
+	showSuppliers();
+}
+function deleteSupplier(id) {
+	"use strict";
+	$.ajax({
+		url: "http://localhost:3000/suppliers/" + id,
+		type: "DELETE"
+	});
+	showSuppliers();
+}
 function init() {
+	"use strict";
 	document.getElementById("add-new-supplier").addEventListener("click", function () {
 		var name = document.getElementById("new-supplier-name").value,
-			lat = document.getElementById("new-supplier-lat").value,
-			long = document.getElementById("new-supplier-long").value;
-		addSupplier(name, lat, long);
+			address = document.getElementById("new-supplier-address").value;
+		addSupplier(name, address);
 	}, false);
 	showSuppliers();
 }
