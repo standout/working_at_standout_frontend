8fc6b1708125f1818e2e09a7a478e9c282c93e7c
Julia Sivartsson <jsigc09@student.lnu.se>
2016-09-01 15:49:01 +0200

    Show standard map with Google Maps

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// JavaScript Document
+var app = {
+
+	init: function(){
+		var map;
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+	}
+}
+
+window.onload = app.init;
