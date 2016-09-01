18de269f5e4ad10291406cc086cddb80f822376a
Julia Sivartsson <jsigc09@student.lnu.se>
2016-09-01 21:53:54 +0200

    Creating new supplier is almost working, not all done yet though

--- app.js
+++ app.js
@@ -46,7 +46,139 @@ var App = {
             //Set "See all" as default value
             $("span").children().last().removeClass("categoryLink").addClass("active");
         });
+		
+		//If user presses button to show new supplier form
+		var showFormButton = document.getElementById("showForm");
+		showFormButton.addEventListener('click', function(){
+			App.toggleVisibility(document.getElementById("newSupplier"));
+		});
+		
+		
+		//TEST CREATE FORM
+		var form = document.createElement("form");
+		
+		var div1 = document.createElement("div");
+		div1.setAttribute("class", "form-group");
+		var nameLabel = document.createElement("label");
+		nameLabel.innerHTML = "Name :";
+		var nameInput = document.createElement("input");
+		nameInput.setAttribute("type", "text");
+		nameInput.setAttribute("id", "newName");
+		div1.appendChild(nameLabel);
+		div1.appendChild(nameInput);
+		
+		var div2 = document.createElement("div");
+		div2.setAttribute("class", "form-group");
+		var addressLabel = document.createElement("label");
+		addressLabel.innerHTML = "Address :";
+		var addressInput = document.createElement("input");
+		addressInput.setAttribute("type", "text");
+		addressInput.setAttribute("id", "newAddress");
+		div2.appendChild(addressLabel);
+		div2.appendChild(addressInput);
+		
+		var div3 = document.createElement("div");
+		div3.setAttribute("class", "form-group");
+		var phoneLabel = document.createElement("label");
+		phoneLabel.innerHTML = "Phone :";
+		var phoneInput = document.createElement("input");
+		phoneInput.setAttribute("type", "text");
+		phoneInput.setAttribute("id", "newPhone");
+		div3.appendChild(phoneLabel);
+		div3.appendChild(phoneInput);
+		
+		var div4 = document.createElement("div");
+		div4.setAttribute("class", "form-group");
+		var categoryLabel = document.createElement("label");
+		categoryLabel.innerHTML = "Category :";
+		//Fill select list with categories
+		var categoryInput = document.createElement("select");
+		categoryInput.setAttribute("id", "newSupplierCategory");
+		
+		App.categoriesArray.forEach(function(category, i){
+			if(category !== 'See all'){
+				var opt = document.createElement('option');
+				opt.id = i;
+				opt.value = category;
+				opt.innerHTML = category;
+				categoryInput.appendChild(opt);
+			}
+		});
+		div4.appendChild(categoryLabel);
+		div4.appendChild(categoryInput);
+		
+		var createButton = document.createElement("button");
+		createButton.setAttribute("id", "createNewSupplier");
+		createButton.setAttribute("class", "btn btn-primary");
+		createButton.innerHTML = "Create new supplier";
+		
+		form.appendChild(div1);
+		form.appendChild(div2);
+		form.appendChild(div3);
+		form.appendChild(div4);
+		form.appendChild(createButton);
+		
+		document.getElementById("newSupplier").appendChild(form);
+		
+		
+
+		
+		//If user presses button to create new supplier
+		var createSupplierButton = document.getElementById("createNewSupplier");
+		createSupplierButton.addEventListener('click', function(){
+			var name = document.getElementById("newName").value;
+			var address = document.getElementById("newAddress").value;
+			var phone = document.getElementById("newPhone").value;
+			var category = document.getElementById("newSupplierCategory");
+			var categoryId = category[category.selectedIndex].id;
+			
+			//If values are empty
+			switch("") {
+				case name:
+					alert("Name cannot be empty!");
+					break;
+				case address:
+					alert("Address cannot be empty!");
+					break;
+				case phone:
+					alert("Phone number cannot be empty!");
+					break;
+				case category:
+					alert("You must choose a category");
+					break;
+				default:
+				    var url = "https://maps.googleapis.com/maps/api/geocode/json";
+					url += "?address=" + address;
+					url += "&key=AIzaSyCoa-xvr_TPQiur6N9GbVdV9WV5GRf6ffo";
+					$.get(url, function (response) {
+						if(response.status === "OK"){
+							var location = response.results[0].geometry.location;
+							App.createNewSupplier(name, address, phone, categoryId, location.lat, location.lng);
+						}
+					});
+			}
+		});
 	},
+	
+	createNewSupplier: function(name, address, phone, category, lat, long){
+		console.log(category);
+		$.post("http://localhost:3000/suppliers", {
+			name: name,
+			address: address,
+			phone: phone,
+			category: category,
+			latitude: lat,
+			longitude: long
+		});
+		location.reload();
+	},
+	
+	toggleVisibility: function(e) {
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    },
 	
 	resetMap: function(){
         App.map.setView([App.defaultLat, App.defaultLong], App.defaultZoom);
