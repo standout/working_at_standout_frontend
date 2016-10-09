3bc8095fa013a85cbf55c36b545c8f1fe8852c4d
Kenneth Eriksson <eriksson.r.k@gmail.com>
2016-10-09 20:53:43 +0200

    Added delete and update functionality

--- public/js/app.js
+++ public/js/app.js
@@ -4,20 +4,34 @@
 Labels = {
 	name:'Labels',
 	List: [],
+	Data: {},
 	create: function(label) {
 		let Obj = {
 			Parent: this,
-			data: {name:label.toLowerCase()},
+			Data: {name:label.toLowerCase()},
 			callBack: this.callBack
 		}
 		label = new Data(Obj)
 		label.save()
 	},
-	read: function(Id) {},
-	update: function(Id) {},
-	delete: function(Id) {},
+	read: function(Obj) {},
+	update: function(id,label) {
+		if (this.Data[id] === undefined) {
+			return false
+		}
+		this.Data[id].update({name:label.toLowerCase()})
+		this.Data[id].save()
+	},
+	delete: function(id) {
+		if (this.Data[id] === undefined) {
+			return false
+		}
+		this.Data[id].delete()
+	},
 	callBack: function(Obj) {
-		console.log(Obj)
+		Obj.Data.id = Obj.Response.id
+		Labels.Data[Obj.Response.id] = Obj.Data
+		Labels.List[Obj.Response.id] = Obj.Response.name
 	}
 	
 }
@@ -32,47 +46,58 @@ Labels = {
  * @return none
  */
 function Data(Obj) {
-	this.data = Obj.data
+	this.Data = Obj.Data
 	this.Parent = Obj.Parent
 	this.callBack = Obj.callBack
 	this.id = false;
+	this.update = function (data) {
+		this.Data = data
+	}
+	this.delete = function() {
+		this.Data = undefined
+		this.save()
+	}
 	this.save = function() {
 		let Opts = {
 			url : '/'+this.Parent.name,
-			method: '',
-			body: '',
-			done: this.callBack
+			method: undefined,
+			body: this.Data,
+			callBack: this.callBack
 		}
-		if (this.data !== undefined) {
+		if (this.Data !== undefined) {
 			// Data isn't empty - create / update
 			if (this.id === false) {
 				Opts.method = 'POST'
-				Opts.body = this.data
+				
 			} else {
 				Opts.url += '/'+this.id
 				Opts.method = 'PUT'
-				Opts.body = {
-					id: this.id,
-					name:this.name
-				}
+				Opts.body.id = this.id
 			}
-		} else if (this.data === undefined && this.id !== false) {
+		} else if (this.Data === undefined && this.id !== false) {
 			// We have id and empty data, send delete
 			Opts.url += '/'+this.id
 			Opts.method = 'DELETE'
 		}
+		if (Opts.method === undefined) {
+			return false
+		}
 		this.xhr(Opts)
+		return true;
 	}
 	this.xhr = function(Opts) {
-	  var xhr = new XMLHttpRequest();
-	  xhr.open(Opts.method, Opts.url);
-	  xhr.setRequestHeader('Content-Type', 'application/json');
-	  xhr.onload = function() {
-	    if (xhr.status > 200 && xhr.status <= 299) {
-	        Opts.done(JSON.parse(xhr.responseText))
-	    }
-	  }
-	  xhr.send(JSON.stringify(Opts.body))		
+		Parent = this
+		var xhr = new XMLHttpRequest();
+		xhr.open(Opts.method, Opts.url);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.onload = function() {
+			if (xhr.status >= 200 && xhr.status <= 299) {
+				Rsp = JSON.parse(xhr.responseText)
+				Opts.callBack({Response: Rsp ,Data: Parent})
+				return Rsp 
+			}
+		}
+		xhr.send(JSON.stringify(Opts.body))		
 	}
 }
 
