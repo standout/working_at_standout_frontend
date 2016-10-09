acde882fff65f57ef7608a5160735c771f0de904
Kenneth Eriksson <eriksson.r.k@gmail.com>
2016-10-09 18:10:23 +0200

    Added app.js, Added basic object for labels

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+/* Objects */
+
+/* Labels Object */
+Labels = {
+	name:'Labels',
+	List: [],
+	create: function() {},
+	read: function() {},
+	update: function() {},
+	delete: function() {}
+	
+}
+
+/* Suppliers Object */
