df62f9a955d14b3b7c87d89a6749b06156b4691a
Kenneth Eriksson <eriksson.r.k@gmail.com>
2016-10-09 18:43:20 +0200

    Added Label object, Added XHR function

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,63 @@
 Labels = {
 	name:'Labels',
 	List: [],
-	create: function() {},
-	read: function() {},
-	update: function() {},
-	delete: function() {}
+	create: function(label) {
+		label = new Label(label)
+		id = label.save()
+		this.List[id] = label
+	},
+	read: function(Id) {},
+	update: function(Id) {},
+	delete: function(Id) {}
 	
 }
 
+/* Label object */
+function Label(label) {
+	this.name = label.toLowerCase()
+	this.id = false;
+	this.save = function() {
+		let Opts = {
+			url : '/labels',
+			method: '',
+			body: '',
+			done: this.completed
+		}
+		if (this.id === false) {
+			Opts.method = 'POST'
+			Opts.body = {name: this.name}
+		} else {
+			Opts.method = 'PUT'
+			Opts.body = {
+				id: this.id,
+				name:this.name
+			}
+		}
+
+		makeRequest(Opts)
+	}
+	this.completed = function(rsp) {
+		console.log(rsp)
+	}
+}
+
 /* Suppliers Object */
+
+/* Functions */
+
+/* Makes the request to API
+ * @param {object} Params for request
+ * @return {string} raw json data
+ */
+
+function makeRequest(Opts) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(Opts.method, Opts.url);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function() {
+    if (xhr.status > 200 && xhr.status <= 299) {
+        Opts.done(JSON.parse(xhr.responseText))
+    }
+  }
+  xhr.send(JSON.stringify(Opts.body))
+}
