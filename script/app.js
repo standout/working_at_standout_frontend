2b4b956c07188929010f3ac97ee7835dc13fbee3
Alexander Gunnarsson <tim.alexander.gunnarsson@gmail.com>
2016-10-18 04:14:03 +0200

    Add methods for network requests

--- script/app.js
+++ script/app.js
@@ -1,21 +1,34 @@
-// Internal API
+// Network request methods
 
-function addSupplier()
-{
-
+function request(method, url, optionalJson) {
+	return new Promise(function (resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.onload = resolve;
+		xhr.onerror = reject;
+		if (optionalJson) {
+			xhr.setRequestHeader("Content-type", "application/json");
+			optionalJson = JSON.stringify(optionalJson);
+		}
+		xhr.send(optionalJson);
+	});
 }
 
-function getSuppliers()
-{
-
+function requestJSON(url) {
+	return request('GET', url)
+		.then(function (e) {
+			return JSON.parse(e.target.response);
+		});
 }
 
-function updateSupplier()
-{
-
+function postSupplier(supplier) {
+	request('POST', 'http://localhost:3000/suppliers', supplier);
 }
 
-function deleteSupplier()
-{
+function putSupplier(supplier) {
+	request('PUT', 'http://localhost:3000/suppliers/' + supplier.id, supplier);
+}
 
+function requestSuppliers() {
+	return requestJSON('http://localhost:3000/suppliers');
 }
