8f83b57bc9f681df5d5f01c2b157c696f4133d40
Kushtrim Abdiu <kushtrimabdiu@Kushtrims-MBP-2.lan>
2016-12-16 16:41:50 +0100

    Moving the directives from the homeController into a separate file only for directives

--- js/controllers/homecontroller.js
+++ js/controllers/homecontroller.js
@@ -21,26 +21,3 @@ function($scope, $rootScope, $location, $mdToast, $mdDialog, $http, Supplier) {
     }
 
 }])
-
-//DIRECTIVES
-.directive('suppliersView', function() {
-    return {
-    	restrict:'E',
-    	templateUrl:'views/suppliers-view.html',
-    	controller: 'suppliersController'
- 	};
-})
-.directive('newSupplier', function() {
-    return {
-    	restrict:'E',
-    	templateUrl:'views/new-supplier.html',
-    	controller: 'newSupplierController'
- 	};
-})
-.directive('mapView', function() {
-    return {
-    	restrict:'E',
-    	templateUrl:'views/map-view.html',
-    	controller: 'mapOverviewController'
- 	};
-})
