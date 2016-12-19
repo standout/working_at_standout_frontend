fa0a5d188071528a92c866ba348a97433f4b0255
Kushtrim Abdiu <kushtrimabdiu@Kushtrims-MBP-2.lan>
2016-12-19 20:34:04 +0100

    Edit the supplier address will change the coordinates appropriatelly too

--- js/controllers/supplierController.js
+++ js/controllers/supplierController.js
@@ -1,6 +1,6 @@
 angular.module('Standout')
-.controller('supplierController', ['$scope', '$rootScope', '$location', '$mdToast', '$mdDialog', '$http','Supplier',
-function($scope, $rootScope, $location, $mdToast, $mdDialog, $http, Supplier) {
+.controller('supplierController', ['$scope', '$rootScope', '$location', '$mdToast', '$mdDialog', '$http','Supplier','Map',
+function($scope, $rootScope, $location, $mdToast, $mdDialog, $http, Supplier, Map) {
     /**
      * supplier - variable that holds the supplier object
      */
@@ -24,17 +24,28 @@ function($scope, $rootScope, $location, $mdToast, $mdDialog, $http, Supplier) {
      */
     $scope.updateSupplier = () => {
        const supplier = new Supplier();
-       supplier.updateSupplier($rootScope.supplier, function(status) {
-           if (status == 200) {
-               //the update was successful
-               $mdToast.show($mdToast.simple().content("SUPPLIER UPDATED SUCCESFULLY"));
-               $location.path("/#");
-           }
-           else {
-               //the update went wrong
-                $mdToast.show($mdToast.simple().content("SOMETHING WENT WRONG!"));
-           }
+       const map = new Map();
+       //we have to also update the coordinates therefore we 
+       //have to check the address once more and update its coordinates
+       map.getCoordinatesForAddress($rootScope.supplier.address, function(coordinates) {
+            //append the coordinates to the newSupplier object
+            $rootScope.supplier['longitude'] = coordinates.longitude;
+            $rootScope.supplier['latitude'] = coordinates.latitude;
+
+            supplier.updateSupplier($rootScope.supplier, function(status) {
+                if (status == 200) {
+                    //the update was successful
+                    $mdToast.show($mdToast.simple().content("SUPPLIER UPDATED SUCCESFULLY"));
+                    $location.path("/#");
+                }
+                else {
+                    //the update went wrong
+                        $mdToast.show($mdToast.simple().content("SOMETHING WENT WRONG!"));
+                }
+            });
+
        });
+       
     }
 
     /**
