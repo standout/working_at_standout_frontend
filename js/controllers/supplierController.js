d85e321e9955707ce6701bec555c99ad4259f73d
Kushtrim Abdiu <kushtrimabdiu@Kushtrims-MBP-2.lan>
2016-12-20 01:35:10 +0100

    Prevent user from editing the address and entering a wrong address

--- js/controllers/supplierController.js
+++ js/controllers/supplierController.js
@@ -29,21 +29,26 @@ function($scope, $rootScope, $location, $mdToast, $mdDialog, $http, Supplier, Ma
        //have to check the address once more and update its coordinates
        map.getCoordinatesForAddress($rootScope.supplier.address, function(coordinates) {
             //append the coordinates to the newSupplier object
-            $rootScope.supplier['longitude'] = coordinates.longitude;
-            $rootScope.supplier['latitude'] = coordinates.latitude;
+            if (coordinates.status === 'found') {
+                $rootScope.supplier['longitude'] = coordinates.longitude;
+                $rootScope.supplier['latitude'] = coordinates.latitude;
 
-            supplier.updateSupplier($rootScope.supplier, function(status) {
-                if (status == 200) {
-                    //the update was successful
-                    $mdToast.show($mdToast.simple().content("SUPPLIER UPDATED SUCCESFULLY"));
-                    $location.path("/#");
-                }
-                else {
-                    //the update went wrong
-                        $mdToast.show($mdToast.simple().content("SOMETHING WENT WRONG!"));
-                }
-            });
-
+                supplier.updateSupplier($rootScope.supplier, function(status) {
+                    if (status == 200) {
+                        //the update was successful
+                        $mdToast.show($mdToast.simple().content("SUPPLIER UPDATED SUCCESFULLY"));
+                        $location.path("/#");
+                    }
+                    else {
+                        //the update went wrong
+                            $mdToast.show($mdToast.simple().content("SOMETHING WENT WRONG!"));
+                    }
+                });
+            }
+            else {
+                $mdToast.show($mdToast.simple().content("ADDRESS WAS WRONG!"));
+                $scope.newSupplier.address = ""; //empty out the address
+            }
        });
        
     }
